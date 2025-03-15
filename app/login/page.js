d149b258621d3dfd58b8d8ff5c27bc941bd4a80c
HarshGeed <harshgeed07@gmail.com>
2025-03-15 17:52:19 +0530

    Google oAuth btn setup

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+import SignIn from "@/Components/Googlebtn";
+
+export default function Login() {
+  return (
+    <>
+      <div className="border-2 border-black w-[12rem] flex justify-center items-center mx-auto mt-[5rem]">
+        <SignIn />
+      </div>
+    </>
+  );
+}
